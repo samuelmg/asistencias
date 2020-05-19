3b2c7a7b8ca6488aff6de47406b38a4a0577a90f
Samuel Mercado <samuel.mg@protonmail.com>
2020-05-19 18:17:30 -0500

    Implementa tabler en eventoIndex

--- resources/views/eventos/eventoIndex.blade.php
+++ resources/views/eventos/eventoIndex.blade.php
@@ -1,39 +1,43 @@
-@extends('layouts.app')
+@extends('layouts.tabler')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Listado de Eventos</div>
-
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Evento</th>
-                                <th>Tipo</th>
-                                <th>No. Asistentes</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($eventos as $evento)
-                                <tr>
-                                    <td>
-                                        {{ $evento->nombre }}
-                                        <a href="{{ route('evento.show', $evento->id) }}">
-                                            <i class="fas fa-info-circle fa-2x"></i>
-                                        </a>
-                                    </td>
-                                    <td>{{ $evento->tipo_evento }}</td>
-                                    <td></td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+<div class="page-header">
+    <div class="row align-items-center">
+      <div class="col-auto">
+        <h2 class="page-title">
+          Eventos
+        </h2>
+      </div>
     </div>
+</div>
+
+<div class="box">
+  <div class="card">
+    <div class="table-responsive">
+      <table class="table table-vcenter card-table">
+        <thead>
+          <tr>
+              <th>Evento</th>
+              <th>Tipo</th>
+              <th>No. Asistentes</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($eventos as $evento)
+                <tr>
+                    <td>
+                        {{ $evento->nombre }}
+                        <a href="{{ route('evento.show', $evento->id) }}">
+                            <i class="fas fa-info-circle fa-2x"></i>
+                        </a>
+                    </td>
+                    <td class="text-muted">{{ $evento->tipo_evento }}</td>
+                    <td class="text-muted"></td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
 @endsection
